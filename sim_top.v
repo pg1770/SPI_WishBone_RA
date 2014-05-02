354c105a35ba64d20d5bc3809a8a489978374205
bodnartibi <piratetibi@gmail.com>
2014-05-03 01:41:26 +0200

    mukodik: mem sim fuggvenyek

--- sim_top.v
+++ sim_top.v
@@ -1,4 +1,4 @@
-`timescale 1ns / 1ps //32
+`timescale 100ns / 1000ps //32
 
 module sim_top;
 
@@ -15,6 +15,24 @@ module sim_top;
 	wire [31:0] DAT_O;
 	wire ACK_O;
 
+	
+wire mem_o;
+reg mem_cs_n;
+reg mem_si;
+
+reg [23:0] mem_beshift; 
+
+wire shift = 23'b00000011;
+
+	M25AA010A mem(
+	.SI(mem_si),
+	.SCK(CLK_I),
+	.CS_N(mem_cs_n),
+	.WP_N(1),
+	.HOLD_N(1),
+	.RESET(RST_I),
+	.SO(mem_O)
+	);
 	// Instantiate the Unit Under Test (UUT)
 	top uut (
 		.ADR_I(ADR_I),
@@ -27,6 +45,185 @@ module sim_top;
 		.DAT_O(DAT_O),
 		.ACK_O(ACK_O)
 	);
+	
+	
+	task mem_write_enable();
+	begin
+		// 8 bit instruction, 16 bit dont care
+		mem_beshift <= {8'b00000110,8'd0, 8'b00000000 };
+		
+		#1
+		mem_cs_n <= 0;
+		mem_beshift <= {mem_beshift[22:0], mem_beshift[23]};
+		mem_si <= mem_beshift[23];
+		#2
+		mem_beshift <= {mem_beshift[22:0], mem_beshift[23]};
+		mem_si <= mem_beshift[23];
+		#2
+		mem_beshift <= {mem_beshift[22:0], mem_beshift[23]};
+		mem_si <= mem_beshift[23];
+		#2
+		mem_beshift <= {mem_beshift[22:0], mem_beshift[23]};
+		mem_si <= mem_beshift[23];
+		#2
+		mem_beshift <= {mem_beshift[22:0], mem_beshift[23]};
+		mem_si <= mem_beshift[23];
+		#2
+		mem_beshift <= {mem_beshift[22:0], mem_beshift[23]};
+		mem_si <= mem_beshift[23];
+		#2
+		mem_beshift <= {mem_beshift[22:0], mem_beshift[23]};
+		mem_si <= mem_beshift[23];
+		#2
+		mem_beshift <= {mem_beshift[22:0], mem_beshift[23]};
+		mem_si <= mem_beshift[23];
+
+		#2
+		mem_cs_n <= 1;
+		#1
+		mem_si <= 0;
+		
+	end
+	endtask
+	
+	//felfutóelnel hivodjon meg
+	//status regiszter olvasasa
+	task mem_status_read();
+	begin
+		// 8 bit instruction, 16 bit dont care
+		mem_beshift <= {8'b00000101,8'd0, 8'b00000000 };
+		
+		#1
+		mem_cs_n <= 0;
+		mem_beshift <= {mem_beshift[22:0], mem_beshift[23]};
+		mem_si <= mem_beshift[23];
+		#2
+		mem_beshift <= {mem_beshift[22:0], mem_beshift[23]};
+		mem_si <= mem_beshift[23];
+		#2
+		mem_beshift <= {mem_beshift[22:0], mem_beshift[23]};
+		mem_si <= mem_beshift[23];
+		#2
+		mem_beshift <= {mem_beshift[22:0], mem_beshift[23]};
+		mem_si <= mem_beshift[23];
+		#2
+		mem_beshift <= {mem_beshift[22:0], mem_beshift[23]};
+		mem_si <= mem_beshift[23];
+		#2
+		mem_beshift <= {mem_beshift[22:0], mem_beshift[23]};
+		mem_si <= mem_beshift[23];
+		#2
+		mem_beshift <= {mem_beshift[22:0], mem_beshift[23]};
+		mem_si <= mem_beshift[23];
+		#2
+		mem_beshift <= {mem_beshift[22:0], mem_beshift[23]};
+		mem_si <= mem_beshift[23];
+		//8 volt
+		//varjuk az adatot
+		#16
+		#2
+		mem_cs_n <= 1;
+		#1
+		mem_si <= 0;
+		
+	end
+	endtask
+
+
+	task mem_write(input [6:0] addr_w, input [7:0] data_w);
+	begin
+		
+		// instruction, write addres, data
+		mem_beshift <= {8'b00000010, 1'b0, addr_w, data_w };
+		
+		#1
+		mem_cs_n = 0;
+		mem_beshift <= {mem_beshift[22:0], mem_beshift[23]};
+		mem_si <= mem_beshift[23];
+		#2
+		mem_beshift <= {mem_beshift[22:0], mem_beshift[23]};
+		mem_si <= mem_beshift[23];
+		#2
+		mem_beshift <= {mem_beshift[22:0], mem_beshift[23]};
+		mem_si <= mem_beshift[23];
+		#2
+		mem_beshift <= {mem_beshift[22:0], mem_beshift[23]};
+		mem_si <= mem_beshift[23];
+		#2
+		mem_beshift <= {mem_beshift[22:0], mem_beshift[23]};
+		mem_si <= mem_beshift[23];
+		#2
+		mem_beshift <= {mem_beshift[22:0], mem_beshift[23]};
+		mem_si <= mem_beshift[23];
+		#2
+		mem_beshift <= {mem_beshift[22:0], mem_beshift[23]};
+		mem_si <= mem_beshift[23];
+		#2
+		mem_beshift <= {mem_beshift[22:0], mem_beshift[23]};
+		mem_si <= mem_beshift[23];
+		#2
+		mem_beshift <= {mem_beshift[22:0], mem_beshift[23]};
+		mem_si <= mem_beshift[23];
+		#2
+		mem_beshift <= {mem_beshift[22:0], mem_beshift[23]};
+		mem_si <= mem_beshift[23];
+		//10  volt
+		#2
+		mem_beshift <= {mem_beshift[22:0], mem_beshift[23]};
+		mem_si <= mem_beshift[23];
+		#2
+		mem_beshift <= {mem_beshift[22:0], mem_beshift[23]};
+		mem_si <= mem_beshift[23];
+		#2
+		mem_beshift <= {mem_beshift[22:0], mem_beshift[23]};
+		mem_si <= mem_beshift[23];
+		#2
+		mem_beshift <= {mem_beshift[22:0], mem_beshift[23]};
+		mem_si <= mem_beshift[23];
+		#2
+		mem_beshift <= {mem_beshift[22:0], mem_beshift[23]};
+		mem_si <= mem_beshift[23];
+		#2
+		mem_beshift <= {mem_beshift[22:0], mem_beshift[23]};
+		mem_si <= mem_beshift[23];
+		#2
+		mem_beshift <= {mem_beshift[22:0], mem_beshift[23]};
+		mem_si <= mem_beshift[23];
+		#2
+		mem_beshift <= {mem_beshift[22:0], mem_beshift[23]};
+		mem_si <= mem_beshift[23];
+		#2
+		mem_beshift <= {mem_beshift[22:0], mem_beshift[23]};
+		mem_si <= mem_beshift[23];
+		#2
+		mem_beshift <= {mem_beshift[22:0], mem_beshift[23]};
+		mem_si <= mem_beshift[23];
+		//20  volt
+		#2
+		mem_beshift <= {mem_beshift[22:0], mem_beshift[23]};
+		mem_si <= mem_beshift[23];
+		#2
+		mem_beshift <= {mem_beshift[22:0], mem_beshift[23]};
+		mem_si <= mem_beshift[23];
+		#2
+		mem_beshift <= {mem_beshift[22:0], mem_beshift[23]};
+		mem_si <= mem_beshift[23];
+		#2
+		mem_beshift <= {mem_beshift[22:0], mem_beshift[23]};
+		mem_si <= mem_beshift[23];
+			
+		#2
+		
+		mem_cs_n <= 1;
+		
+		
+		#1
+		mem_si <= 0;
+		
+	end
+	endtask
+	
+	
 
 	task bus_read(input [7:0] addr_r); //Busz olvasÃ¡s taskja
 	begin
@@ -75,19 +272,34 @@ module sim_top;
 		DAT_I = 0;
 		RST_I = 0;
 		STB_I = 0;
+		
+		mem_cs_n = 1;
 
 		// Wait 100 ns for global reset to finish
-		#100;
-
+		#50
+		RST_I = 1;
+		#20
+		RST_I = 0;
+		#5
 		// Add stimulus here
+		#10
+		mem_write_enable();
+		#10
+		mem_write(7'b0000000, 8'b11111111);
+		# 50
+
+		mem_status_read ();
+		
+		/*
+		bus_read(1);
+		bus_write(1,32'hABCD);
 		bus_read(1);
 		bus_write(1,32'hFEDC);
 		bus_read(1);
+		*/
 	end
 
 	always #1 CLK_I = ~CLK_I;
 
 endmodule
-
-
 
